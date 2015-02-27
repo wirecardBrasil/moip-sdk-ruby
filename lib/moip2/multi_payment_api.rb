637e4799e37beb5639e795ab4fed90828fa7f3a1
Danillo Souza <danillo.souza@moip.com.br>
2015-02-27 18:26:37 -0300

    adding GET os multiPayments

--- lib/moip2/multi_payment_api.rb
+++ lib/moip2/multi_payment_api.rb
@@ -13,5 +13,9 @@ module Moip2
     def create(multi_order_id, payment)
       Resource::Payment.new client.post(base_path(multi_order_id), payment)
     end
+
+    def show(multi_payment_id)
+      Resource::MultiPayment.new client.get("/v2/multipayments/#{multi_payment_id}")
+    end
   end
 end
