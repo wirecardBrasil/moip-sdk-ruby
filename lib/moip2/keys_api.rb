ef7c17dd359754268b138fac714fc95c31819b8d
Pedro Alves <pedro.alves@moip.com.br>
2015-05-06 10:13:21 -0300

    keys alright

--- lib/moip2/keys_api.rb
+++ lib/moip2/keys_api.rb
@@ -5,8 +5,15 @@ module Moip2
 		attr_reader :client
 		
 		def initialize(client)
-			
+			@client = client
 		end
 	
+		def base_path
+			"/v2/keys"
+		end
+		
+		def show(id)
+			Resource::Keys.new client, client.get("#{base_path}/#{id}")
+		end
 	end
 end
