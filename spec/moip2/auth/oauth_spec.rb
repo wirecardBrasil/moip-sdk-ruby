e87bfd8c75a5abbcbccd96b1794f736b6cbae164
Joao Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2017-08-17 15:23:14 -0300

    tests: updated OAuth keys to be v2 keys

--- spec/moip2/auth/oauth_spec.rb
+++ spec/moip2/auth/oauth_spec.rb
@@ -1,9 +1,9 @@
 describe Moip2::Auth::OAuth do
-  let(:oauth_with_text) { described_class.new "OAuth 1tldio91gi74r34zv30d4saz8yuuws5" }
-  let(:oauth_without_text) { described_class.new "1tldio91gi74r34zv30d4saz8yuuws5" }
+  let(:oauth_with_text) { described_class.new "OAuth 9fdc242631454d4c95d82e27b4127394_v2" }
+  let(:oauth_without_text) { described_class.new "9fdc242631454d4c95d82e27b4127394_v2" }
 
   describe ".header" do
-    it { expect(oauth_with_text.header).to eq "OAuth 1tldio91gi74r34zv30d4saz8yuuws5" }
-    it { expect(oauth_without_text.header).to eq "OAuth 1tldio91gi74r34zv30d4saz8yuuws5" }
+    it { expect(oauth_with_text.header).to eq "OAuth 9fdc242631454d4c95d82e27b4127394_v2" }
+    it { expect(oauth_without_text.header).to eq "OAuth 9fdc242631454d4c95d82e27b4127394_v2" }
   end
 end
