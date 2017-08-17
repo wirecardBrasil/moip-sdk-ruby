7b994c0231c7d990276cb72774206562429e18e0
Joao Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2017-08-17 14:23:45 -0300

    test: updated OAuth key in tests
    The key contained in the tests wasn't working anymore, so it was replaced by a new (functional)
    key.

--- spec/moip2/auth/oauth_spec.rb
+++ spec/moip2/auth/oauth_spec.rb
@@ -1,9 +1,9 @@
 describe Moip2::Auth::OAuth do
-  let(:oauth_with_text) { described_class.new "OAuth d63tz2xwyu0ewrembove4j5cbv2otpd" }
-  let(:oauth_without_text) { described_class.new "d63tz2xwyu0ewrembove4j5cbv2otpd" }
+  let(:oauth_with_text) { described_class.new "OAuth 1tldio91gi74r34zv30d4saz8yuuws5" }
+  let(:oauth_without_text) { described_class.new "1tldio91gi74r34zv30d4saz8yuuws5" }
 
   describe ".header" do
-    it { expect(oauth_with_text.header).to eq "OAuth d63tz2xwyu0ewrembove4j5cbv2otpd" }
-    it { expect(oauth_without_text.header).to eq "OAuth d63tz2xwyu0ewrembove4j5cbv2otpd" }
+    it { expect(oauth_with_text.header).to eq "OAuth 1tldio91gi74r34zv30d4saz8yuuws5" }
+    it { expect(oauth_without_text.header).to eq "OAuth 1tldio91gi74r34zv30d4saz8yuuws5" }
   end
 end
