58de40f591b1c54ee30954351829a7f351eb87ce
Joao Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2017-08-22 09:52:58 -0300

    feat(accounts): added #exists?

--- lib/moip2/accounts_api.rb
+++ lib/moip2/accounts_api.rb
@@ -13,5 +13,11 @@ module Moip2
     def create(account)
       Resource::Account.new client, client.post(base_path, account)
     end
+
+    def exists?(tax_document = "")
+      response = client.get("#{base_path}/exists?tax_document=#{tax_document}")
+
+      response.code.to_i == 200
+    end
   end
 end
