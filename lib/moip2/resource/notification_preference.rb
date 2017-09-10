8672f3073c18d94cfcd089c4b827c5936c3c1117
Nielson Rolim <nielson.rolim@gmail.com>
2017-09-10 10:40:50 -0300

    Fix Moip2::Resource::NotificationPreference - extended SimpleDelegator

--- lib/moip2/resource/notification_preference.rb
+++ lib/moip2/resource/notification_preference.rb
@@ -1,6 +1,6 @@
 module Moip2
   module Resource
-    class NotificationPreference
+    class NotificationPreference < SimpleDelegator
       attr_reader :client
 
       def initialize(client, response)
