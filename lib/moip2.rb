90438408c7ed6111e8607a83d765315f798484da
Nielson Rolim <nielson.rolim@gmail.com>
2017-09-10 10:31:17 -0300

    Added notification preferences requires to moip2 main file

--- lib/moip2.rb
+++ lib/moip2.rb
@@ -17,6 +17,7 @@ require "moip2/resource/invoice"
 require "moip2/resource/keys"
 require "moip2/resource/refund"
 require "moip2/resource/webhooks"
+require "moip2/resource/notification_preference"
 
 require "moip2/response"
 require "moip2/client"
@@ -31,6 +32,7 @@ require "moip2/api"
 require "moip2/keys_api"
 require "moip2/webhooks_api"
 require "moip2/accounts_api"
+require "moip2/notification_preference_api"
 
 require "moip2/exceptions/invalid_enviroment_error"
 
