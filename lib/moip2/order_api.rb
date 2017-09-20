7e66c07ddd83a9777eb0b7bdf0f8a679e931234b
Joao Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2017-09-20 13:41:36 -0300

    feat(order): added `find_all`

--- lib/moip2/order_api.rb
+++ lib/moip2/order_api.rb
@@ -17,5 +17,16 @@ module Moip2
     def show(id)
       Resource::Order.new client, client.get("#{base_path}/#{id}")
     end
+
+    def find_all(limit: nil, offset: nil)
+      params = URI.encode_www_form(limit: limit, offset: offset)
+      path = "#{base_path}?#{params}"
+      response = client.get(path)
+
+      # We need to transform raw JSON in Order objects
+      order_objects = response.orders.map { |order| Resource::Order.new client, order }
+      response.orders = order_objects
+      Resource::Order.new client, response
+    end
   end
 end
