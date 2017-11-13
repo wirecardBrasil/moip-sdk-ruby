fb14e1530b32f23232cc37b6acdb6209e01d15f8
bressani <jr.bressani@hotmail.com>
2017-11-13 13:49:42 -0200

    feat(webhooks): add tests

--- spec/moip2/webhooks_spec.rb
+++ spec/moip2/webhooks_spec.rb
@@ -4,7 +4,24 @@ describe Moip2::WebhooksApi do
   describe "#show" do
     let(:get_webhooks) do
       VCR.use_cassette("get_webhooks") do
-        webhooks_api.show
+        webhooks_api.show("PAY-6B66XQW6CZHH")
+      end
+    end
+
+    context "when shooting request by webhooks" do
+      it { expect(get_webhooks.webhooks[0][:id]).not_to be_nil }
+      it { expect(get_webhooks.webhooks[0][:resource_id]).to eq "PAY-6B66XQW6CZHH" }
+      it { expect(get_webhooks.webhooks[0][:event]).to eq "PAYMENT.SETTLED" }
+      it { expect(get_webhooks.webhooks[0][:url])	.to eq "http://requestb.in/" }
+      it { expect(get_webhooks.webhooks[0][:status]).to eq "CREATED" }
+      it { expect(get_webhooks.webhooks[0][:sent_at]).to eq "2017-11-12T02:20:49.269Z" }
+    end
+  end
+
+  describe "#find_all" do
+    let(:get_webhooks) do
+      VCR.use_cassette("find_all_webhooks") do
+        webhooks_api.find_all
       end
     end
 
@@ -17,4 +34,5 @@ describe Moip2::WebhooksApi do
       it { expect(get_webhooks.webhooks[0][:sent_at]).to eq "May 13, 2015 7:09:06 PM" }
     end
   end
+
 end
